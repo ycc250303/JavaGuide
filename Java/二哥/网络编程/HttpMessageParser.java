efd8ac41f71cb200ea7e801d6565889142c29b05
ycc250303 <2351279@tongji.edu.cn>
2025-10-30 16:44:23 +0800

    Java网络编程

---
+++ Java/二哥/网络编程/HttpMessageParser.java
@@ -0,0 +1,30 @@
+public class HttpMessageParser {
+    
+    
+}
+
+@Data
+public static class Request {
+    /**
+     * 请求方法 GET/POST/PUT/DELETE/OPTION...
+     */
+    private String method;
+    /**
+     * 请求的uri
+     */
+    private String uri;
+    /**
+     * HTTP版本
+     */
+    private String version;
+
+    /**
+     * 请求头
+     */
+    private Map<String, String> headers;
+
+    /**
+     * 请求参数相关
+     */
+    private String message;
+}
